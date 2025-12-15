8588d5853bc5ebc5480916af989e30902d616a37
Prateek Gupta <prateekjpg@gmail.com>
2025-12-15 17:11:15 +0530

    Add initial code for complete jwt auth

---
+++ django_authentication/complete_jwt_auth/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CompleteJwtAuthConfig(AppConfig):
+    name = 'complete_jwt_auth'
--- django_authentication/django_authentication/settings.py
+++ django_authentication/django_authentication/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'main',
+    'complete_jwt_auth',
 ]
 
 MIDDLEWARE = [
